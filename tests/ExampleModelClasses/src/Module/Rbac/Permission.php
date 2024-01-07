715300a4b2db997506659a53097e4f07f1b76fb4
Arkadiusz Kawałko <arek.kawalko@gmail.com>
2024-01-07 15:29:26 +0100

    Adds example classes that extend \RedBeanPHP\SimpleModel class.

---
+++ tests/ExampleModelClasses/src/Module/Rbac/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akawalko\RedbeanExtras\Tests\ExampleModelClasses\src\Module\Rbac;
+
+// when you use php > 8.0 you can extend \RedBeanPHP\TypedModel
+class Permission extends \RedBeanPHP\SimpleModel
+{
+
+}
---
+++ tests/ExampleModelClasses/src/Module/Rbac/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akawalko\RedbeanExtras\Tests\ExampleModelClasses\src\Module\Rbac;
+
+// when you use php > 8.0 you can extend \RedBeanPHP\TypedModel
+class Role extends \RedBeanPHP\SimpleModel
+{
+
+}
