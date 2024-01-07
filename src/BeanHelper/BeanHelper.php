2735b280f5fb6e4d21373680405ce6f6109ae505
Arkadiusz Kawałko <arek.kawalko@gmail.com>
2024-01-07 16:37:27 +0100

    Adds BeanHelper class.

---
+++ src/BeanHelper/BeanHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akawalko\RedbeanExtras\BeanHelper;
+
+use RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper;
+use RedBeanPHP\OODBBean;
+
+class BeanHelper extends SimpleFacadeBeanHelper
+{
+    private array $redbeanTypeIndexedClassNames;
+
+    public function __construct(array $redbeanTypeIndexedClassNames = [])
+    {
+        $this->redbeanTypeIndexedClassNames = $redbeanTypeIndexedClassNames;
+    }
+
+    public function getModelForBean(OODBBean $bean)
+    {
+        $type = $bean->getMeta('type');
+
+        if (!empty($this->redbeanTypeIndexedClassNames) && isset($this->redbeanTypeIndexedClassNames[$type])) {
+            $modelName = $this->redbeanTypeIndexedClassNames[$type]; // already contain type in its path
+            /** @var \RedBeanPHP\SimpleModel $obj */
+            $obj = self::factory($modelName);
+            $obj->loadBean($bean);
+            return $obj;
+        }
+
+        return parent::getModelForBean($bean);
+    }
+}
