dbf7eba55b63b982c474ff2a49c57ae2cf04542b
Arkadiusz Kawałko <arek.kawalko@gmail.com>
2024-01-07 18:29:08 +0100

    Adds example method to model class.

--- tests/ExampleModelClasses/src/Module/User/User.php
+++ tests/ExampleModelClasses/src/Module/User/User.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Akawalko\RedbeanExtras\Tests\ExampleModelClasses\src\Module\User;
 
+use DateTime;
+
 // when you use php > 8.0 you can extend \RedBeanPHP\TypedModel
 class User extends \RedBeanPHP\SimpleModel
 {
+    public const AGE_OF_CONSENT_TO_PURCHASE_ALCOHOL = 18;
 
+    public function canBuyAlcohol(): bool
+    {
+        $now = new DateTime();
+        $birthdate = new DateTime($this->bean->birthdate);
+        $dateInterval = $now->diff($birthdate);
+
+        return (int) $dateInterval->format('%Y') > self::AGE_OF_CONSENT_TO_PURCHASE_ALCOHOL;
+    }
 }
